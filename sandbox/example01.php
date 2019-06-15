f8ec00bd4d75c418f2485052f1ffaadd0701b330
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-06-15 22:17:18 +0530

    Example01

---
+++ sandbox/example01.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Example 01</title>
+	</head>
+	<body>
+		<?php echo "This is my First PHP Script"; ?>
+		<?php echo "<br>"; ?>
+		<?php phpinfo(); ?>
+	</body>
+</html>
