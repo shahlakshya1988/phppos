fbdd79ad4b8b3b513c243ecfbc91237dcca9b5f3
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-06-16 07:05:29 +0530

    Example 03 Comments In PHP

---
+++ sandbox/example03.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Comments In PHP/title>
+	</head>
+	<body>
+	<?php
+		#this is single line comment
+		//this is single line commet 
+		/*
+		Multiple 
+		line 
+		comment
+		*/
+	?>
+	</body>
+</html>
