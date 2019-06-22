fc223999456c7bcae41da13a9c8b7d6d76868f72
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-06-22 07:07:55 +0530

    Include, Include_once, require, require_once

---
+++ sandbox/example20.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Include / Include Once / Require / Require Once</title>
+</head>
+<body>
+<?php
+    /*
+    echo "<h2>Include</h2>";
+    include "./extra/include_require.php";
+    echo "<h2>Include</h2>";
+    include "./extra/include_require.php";
+    echo "<h2>Include</h2>";
+    include "./extra/include_require.php"; */
+
+    /*echo "<h2>Include</h2>";
+    include_once "./extra/include_require.php";
+    echo "<h2>Include</h2>";
+    include_once "./extra/include_require.php";
+    echo "<h2>Include</h2>";
+    include_once "./extra/include_require.php"; */
+
+
+    /*echo "<h2>Include</h2>";
+    include_once "./e1xtra/include_require.php";
+    echo "<h2>Include</h2>";
+    include_once "./e1xtra/include_require.php";
+    echo "<h2>Include</h2>";
+    include_once "./e1xtra/include_require.php";*/
+
+    /*echo "<h2>Require</h2>";
+    require "./extra/include_require.php";
+    echo "<h2>Require</h2>";
+    require "./extra/include_require.php";
+    echo "<h2>Require</h2>";
+    require "./extra/include_require.php"; */
+
+    /*echo "<h2>Require</h2>";
+    require_once "./extra/include_require.php";
+    echo "<h2>Require</h2>";
+    require_once "./extra/include_require.php";
+    echo "<h2>Require</h2>";
+    require_once "./extra/include_require.php"; */
+    echo "<h2>Require</h2>";
+    require_once "./ex1tra/include_require.php";
+    echo "<h2>Require</h2>";
+    require_once "./ex1tra/include_require.php";
+    echo "<h2>Require</h2>";
+    require_once "./ext1ra/include_require.php";
+
+?>
+<footer>
+    <p>this is footer</p>
+</footer>
+</body>
+</html>
---
+++ sandbox/extra/include_require.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="heading">
+        <h2>Hello, This is From Outside</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero magni, praesentium ex omnis! Quia nisi impedit nobis excepturi amet reprehenderit rem, dolorum provident, temporibus dolore nulla, totam a dicta, magnam eaque fugit numquam incidunt magni eum corporis nemo. Officia at adipisci esse quasi, nemo autem deleniti optio temporibus ducimus illum.</p>
+    </div>
+</div>
