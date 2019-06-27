f6f5d7a39426de65d595fd45a607d6b09ad219c3
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-06-27 07:56:14 +0530

    Retriving Data PDO::FETCH_ASSOC

---
+++ pdobasics/example05.php
@@ -0,0 +1,19 @@
+<?php require "dbconnect.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Retriving Data, FETCH_ASSOC</title>
+</head>
+<body>
+	<?php 
+		$select = $con->prepare("SELECT * FROM `tbl_product` ");
+		$select->execute();
+		while($row = $select->fetch(PDO::FETCH_ASSOC)){
+			echo "<br>";
+			var_dump($row);
+			echo "<hr><br>";
+		}
+	?>
+</body>
+</html>
