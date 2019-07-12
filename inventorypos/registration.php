57466391c120cae0ecbcd42a516e27496a44f675
lakshyapaceinfonet <lakshya@paceinfonet.com>
2019-07-12 10:19:19 +0530

    User Registration Module Completed

--- inventorypos/registration.php
+++ inventorypos/registration.php
@@ -1,92 +1,91 @@
 <?php require_once "./header.php"; ?>
 <?php
-if(isset($_GET["del"])){
+if (isset($_GET["del"])) {
     $userid = trim($_GET["del"]);
     $delete = $pdo->prepare("DELETE FROM `tbl_user` where userid=:userid LIMIT 1");
-    $delete->bindParam(":userid",$userid);
+    $delete->bindParam(":userid", $userid);
     $delete->execute();
     //var_dump($delete);
-    if($delete->rowCount()){
+    if ($delete->rowCount()) {
         ?>
-            <script>
-                window.addEventListener("load",function(){
-                    swal({
-                                        title:"Delete Successfull",
-                                        text:"User Has Been Delete From The System",
-                                        icon:"success",
-                                        button:"Ok",
-                                    });
+        <script>
+            window.addEventListener("load", function() {
+                swal({
+                    title: "Delete Successfull",
+                    text: "User Has Been Delete From The System",
+                    icon: "success",
+                    button: "Ok",
                 });
-            </script>
-        <?php 
+            });
+        </script>
+    <?php
     }
     header("refresh:3;registration.php");
 }
-if(isset($_POST["btnAddUser"])){
-    $txtname= trim($_POST["txtname"]);
-    $txtemail= trim($_POST["txtemail"]);
-    $password= trim($_POST["password"]);
-    $role= trim($_POST["txtSelect_option"]);
+if (isset($_POST["btnAddUser"])) {
+    $txtname = trim($_POST["txtname"]);
+    $txtemail = trim($_POST["txtemail"]);
+    $password = trim($_POST["password"]);
+    $role = trim($_POST["txtSelect_option"]);
 
     // select email from database
     $sel_email = $pdo->prepare("SELECT * FROM `tbl_user` where `useremail` = :useremail");
-    $sel_email->bindParam(":useremail",$txtemail);
+    $sel_email->bindParam(":useremail", $txtemail);
     $sel_email->execute();
-    if(!$sel_email->rowCount()){
+    if (!$sel_email->rowCount()) {
 
-        
+
 
         $insert = $pdo->prepare("INSERT INTO `tbl_user` (`username`,`useremail`,`password`,`role`) values(:username,:useremail,:password,:role)");
-        $insert->bindParam(":username",$txtname);
-        $insert->bindParam(":useremail",$txtemail);
-        $insert->bindParam(":password",$password);
-        $insert->bindParam(":role",$role);
-        if($insert->execute()){
+        $insert->bindParam(":username", $txtname);
+        $insert->bindParam(":useremail", $txtemail);
+        $insert->bindParam(":password", $password);
+        $insert->bindParam(":role", $role);
+        if ($insert->execute()) {
             ?>
             <script>
-                window.addEventListener("load",function(){
+                window.addEventListener("load", function() {
                     swal({
-                        title:"Registration Successfull",
-                        text:"<?php echo $txtname; ?> Has Been Registered",
-                        icon:"success",
-                        button:false,
+                        title: "Registration Successfull",
+                        text: "<?php echo $txtname; ?> Has Been Registered",
+                        icon: "success",
+                        button: false,
                     });
                 });
             </script>
-            
-            <?php
-        }else{
+
+        <?php
+        } else {
             ?>
-             <script>
-                 window.addEventListener("load",function(){
+            <script>
+                window.addEventListener("load", function() {
                     swal({
-                        title:"Registration Unsuccessful",
-                        text:"Please Try Again Later",
-                        icon:"warning",
-                        button:"ok"
+                        title: "Registration Unsuccessful",
+                        text: "Please Try Again Later",
+                        icon: "warning",
+                        button: "ok"
                     });
-                 });
-             </script>
-            <?php 
+                });
+            </script>
+        <?php
             //echo "Registration Unsuccessfull";
         }
-    }else{
-       ?>
-       <script>
-           window.addEventListener("load",function(){
-              swal({
-                  title:"User Email Already Exists",
-                  text:"Please Try Again With Other Email Address",
-                  icon:"warning",
-                  button:"ok"
-              });
-           });
-       </script>
-       <?php 
+    } else {
+        ?>
+        <script>
+            window.addEventListener("load", function() {
+                swal({
+                    title: "User Email Already Exists",
+                    text: "Please Try Again With Other Email Address",
+                    icon: "warning",
+                    button: "ok"
+                });
+            });
+        </script>
+    <?php
     }
     // select email from database
     header("refresh:3;registration.php");
-
 }
 ?>
 <!-- Content Wrapper. Contains page content -->
@@ -160,14 +159,14 @@ if(isset($_POST["btnAddUser"])){
                                 <?php
                                 $select = $pdo->prepare("SELECT * FROM `tbl_user` order by `userid` DESC");
                                 $select->execute();
-                                while($row = $select->fetch(PDO::FETCH_OBJ)){
+                                while ($row = $select->fetch(PDO::FETCH_OBJ)) {
                                     echo "<tr>";
-                                        echo "<td>$row->userid</td>";
-                                        echo "<td>$row->username</td>";
-                                        echo "<td>$row->useremail</td>";
-                                        echo "<td>$row->password</td>";
-                                        echo "<td>$row->role</td>";
-                                        echo "<td> <a href='registration.php?del={$row->userid}' role='button' class='btn btn-danger'> Delete </a> </td>";
+                                    echo "<td>$row->userid</td>";
+                                    echo "<td>$row->username</td>";
+                                    echo "<td>$row->useremail</td>";
+                                    echo "<td>$row->password</td>";
+                                    echo "<td>$row->role</td>";
+                                    echo "<td> <a href='registration.php?del={$row->userid}' role='button' class='btn btn-danger'> Delete </a> </td>";
                                     echo "</tr>";
                                 }
                                 ?>
