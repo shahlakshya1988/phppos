5d4d3a91cf18cc7756dd5a2d0728a8fdc351c3a7
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-07-30 08:39:29 +0530

    121. Billing Invoice in php part 02 Static Invoice

--- inventorypos/invoice.php
+++ inventorypos/invoice.php
@@ -8,6 +8,12 @@
 	//create pdf object
 	$pdf= new fpdf("P","mm","A4");
 	$pdf->AddPage();
+	$pdf->SetFont("Arial","B",20);
+	$pdf->SetFillColor(150,150,150);
+	$pdf->Cell(80,10,"Hello World1",0,1,"C",true,"https://www.google.com");
+	$pdf->SetFillColor(120,120,120);
+	$pdf->Cell(80,10,"Hello World2",1,0,"C",false,"https://www.google.com");
+	$pdf->Cell(80,10,"Hello World3",1,0,"C",true,"https://www.google.com");
 	$pdf->output();
 	
 ?>
