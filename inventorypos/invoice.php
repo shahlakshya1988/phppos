b3d4fddd50f5b02e35ea12b48f1d0b609161bd32
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-07-30 08:56:58 +0530

    122. Billing Invoice in php part 03 Static Invoice

--- inventorypos/invoice.php
+++ inventorypos/invoice.php
@@ -4,16 +4,19 @@
 	// A4 219 mm
 	// default margin 10mm each side 
 	// writable 199 mm 
-	
+	$invoiceNumber = rand(1111,9999);
+	$date = date("d-F-Y");
 	//create pdf object
 	$pdf= new fpdf("P","mm","A4");
 	$pdf->AddPage();
 	$pdf->SetFont("Arial","B",20);
-	$pdf->SetFillColor(150,150,150);
-	$pdf->Cell(80,10,"Hello World1",0,1,"C",true,"https://www.google.com");
-	$pdf->SetFillColor(120,120,120);
-	$pdf->Cell(80,10,"Hello World2",1,0,"C",false,"https://www.google.com");
-	$pdf->Cell(80,10,"Hello World3",1,0,"C",true,"https://www.google.com");
+	$pdf->Cell(189,10,"Inventory POS",1,1,"C",false,"https://www.google.com");
+	$pdf->setFont("Arial","",14);
+	$pdf->Cell(100,10,"My Addres Goes Here ",0,0,"L",false);
+	$pdf->Cell(89,10,"Date# {$date}",1,1,"R",false);
+		$pdf->Cell(100,10,"shahlakshya1988@gmail.com ",0,0,"L",false);
+	$pdf->Cell(89,10,"Invoice No.# {$invoiceNumber}",1,1,"R",false);
+	$pdf->Cell(189,10," ",1,1,false);
 	$pdf->output();
 	
 ?>
