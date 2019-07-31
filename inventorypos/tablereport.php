7f3cf0d8bf53ccb7d007d472bf6a0dc23bcaab29
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-07-31 20:25:01 +0530

    133. Sales Report part03-Tablereport03

--- inventorypos/tablereport.php
+++ inventorypos/tablereport.php
@@ -1,4 +1,22 @@
 <?php require_once "./header.php"; ?>
+<?php
+if(isset($_POST["btnDateFilter"])){
+  $fromdate = $_POST["datepicker_1"];
+  $todate = $_POST["datepicker_2"];
+  $fromdate_timestamp = strtotime($fromdate);
+  $todate_timestamp = strtotime($todate);
+
+
+  $sel_tbl_invoice_query = "SELECT * FROM `tbl_invoice` WHERE `orderdate_timestamp` BETWEEN :fromdate_timestamp and :todate_timestamp"; 
+  $sel_tbl_invoice = $pdo->prepare($sel_tbl_invoice_query);
+  $sel_tbl_invoice->bindParam(":fromdate_timestamp",$fromdate_timestamp);
+  $sel_tbl_invoice->bindParam(":todate_timestamp",$todate_timestamp);
+}else{
+  $sel_tbl_invoice_query = "SELECT * FROM `tbl_invoice`"; 
+  $sel_tbl_invoice = $pdo->prepare($sel_tbl_invoice_query);
+  
+} 
+?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -17,16 +35,22 @@
     <section class="content container-fluid">
       <div class="box box-warning">
         <div class="box-header with-border">
-            <h3 class="box-title">Page Title Goes Here</h3>
+            <?php if(isset($_POST["btnDateFilter"])){ ?>
+              <h3 class="box-title"><?php echo "From ".$_POST["datepicker_1"]." to ".$_POST["datepicker_2"] ?></h3>
+              <?php }else{ ?>
+                <h3 class="box-title">All Sales Report</h3>
+              <?php } ?>
         </div> <!-- div.box-header -->
         <div class="box-body">
             <div class="row">
+              <form action="" method="POST" autocomplete="off">
+                
                 <div class="col-md-5">
                     <div class="input-group date">
                         <div class="input-group-addon">
                             <i class="fa fa-calendar"></i>
                         </div>
-                        <input type="text" class="form-control datepicker pull-right" id="datepicker_1" name="datepicker_1">
+                        <input type="text" class="form-control datepicker pull-right" id="datepicker_1" name="datepicker_1" value="<?php if(isset($_POST["datepicker_1"])){ echo $_POST["datepicker_1"]; }  ?>">
                     </div>
                 </div>
                 <!-- col-md-5 -->
@@ -35,7 +59,7 @@
                       <div class="input-group-addon">
                         <i class="fa fa-calender"></i>
                       </div>
-                      <input type="text" class="form-control datepicker pull-right" id="datepicker_2" name="datepicker_2">
+                      <input type="text" class="form-control datepicker pull-right" id="datepicker_2" name="datepicker_2" value="<?php if(isset($_POST["datepicker_2"])){ echo $_POST["datepicker_2"]; }  ?>">
                     </div>
                 </div>
                 <!-- col-md-5 -->
@@ -45,6 +69,8 @@
                     </div>
                 </div>
                 <!-- div.col-md-2 -->
+                
+              </form>
             </div>
             <!-- div.row -->
             <br><br>
@@ -118,18 +144,36 @@
                        <th>Invoice Id</th>
                        <th>Customer Name</th>
                        <th>Order Date</th>
+                       <th>Sub Total</th>
+                       <th>Tax</th>
+                       <th>Discount</th>
                        <th>Total</th>
                        <th>Paid</th>
+                       <th>Due</th>
+                       <th>Paymenty Type</th>
                     </tr>
                 </thead>
                 <tbody>
-                   <tr>
-                     <td>---</td>
-                     <td>---</td>
-                     <td>---</td>
-                     <td>---</td>
-                     <td>---</td>
-                   </tr>
+                  <?php 
+                    
+
+                    $sel_tbl_invoice->execute();
+                    while($row_tbl_invoice = $sel_tbl_invoice->fetch(PDO::FETCH_OBJ)){
+                      echo "<tr>";
+                        echo "<td>{$row_tbl_invoice->invoice_id}</td>";
+                        echo "<td>{$row_tbl_invoice->customer_name}</td>";
+                        echo "<td>{$row_tbl_invoice->orderdate}</td>";
+                        echo "<td>{$row_tbl_invoice->subtotal}</td>";
+                        echo "<td>{$row_tbl_invoice->tax}</td>";
+                        echo "<td>{$row_tbl_invoice->discount}</td>";
+                        echo "<td>{$row_tbl_invoice->total}</td>";
+                        echo "<td>{$row_tbl_invoice->paid}</td>";
+                        echo "<td>{$row_tbl_invoice->due}</td>";
+                        echo "<td>{$row_tbl_invoice->payment_type}</td>";
+                      echo "</tr>";
+                    }
+                  ?>
+                   
 
                 </tbody>
                 <tfoot>
@@ -139,6 +183,8 @@
                        <th>Order Date</th>
                        <th>Total</th>
                        <th>Paid</th>
+                       <th>Due</th>
+                       <th>Paymenty Type</th>
                     </tr>
                 </tfoot>
             </table>
